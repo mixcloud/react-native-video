51b8e2c0cbe95fb57827c8d84eac9385a47b6fb6
limaAniceto <miguel.aniceto@mixcloud.com>
2020-06-08 14:09:16 +0100

    SetKeepScreenOn

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.accessibility.CaptioningManager;
 import android.widget.FrameLayout;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -223,11 +224,13 @@ class ReactExoplayerView extends FrameLayout implements
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         initializePlayer();
+        setKeepScreenOn(true);    
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
+        setKeepScreenOn(false);
         /* We want to be able to continue playing audio when switching tabs.
          * Leave this here in case it causes issues.
          */
@@ -1188,6 +1191,7 @@ class ReactExoplayerView extends FrameLayout implements
             } else {
                 pausePlayback();
             }
+                setKeepScreenOn(!isPaused);
         }
     }
 
